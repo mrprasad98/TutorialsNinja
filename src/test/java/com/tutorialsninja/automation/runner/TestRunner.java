536a59a27613a3b760715fda25d8fdd56788d3be
Modem Rajendra Prasad <rajendraprasad.modem@gmail.com>
2020-01-11 12:23:00 +0530

    changes done to POM Name

---
+++ src/test/java/com/tutorialsninja/automation/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package com.tutorialsninja.automation.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"classpath:FeatureFiles/Register.feature", "classpath:FeatureFiles/Login.feature", "classpath:FeatureFiles/Search.feature", "classpath:FeatureFiles/Orders.feature"},
+glue={"classpath:com.tutorialsninja.automation.stepdef"},
+plugin={"html:target/cucumber_html_report"}
+
+)
+public class TestRunner {
+
+}
